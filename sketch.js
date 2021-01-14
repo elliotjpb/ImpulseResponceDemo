f952894bc770fca9d8e53798b30058260563435e
Elliot Brown <elliotjpb@gmail.com>
2021-01-14 15:44:22 +0000

    simple text example

--- sketch.js
+++ sketch.js
@@ -2,11 +2,6 @@ let sound, env, cVerb, fft;
 let currentIR = 0;
 let rawImpulse;
 
-var leftSide;
-var rightSide;
-
-let radio;
-
 function preload() {
   // we have included both MP3 and OGG versions of all the impulses/sounds
   soundFormats('mp3', 'ogg');
@@ -21,19 +16,7 @@ function preload() {
 }
 
 function setup() {
-  //main canvas
-  	createCanvas(800, 400);
-  
-  	radio = createRadio();
-  	radio.option('Spring');
-  	radio.option('Concrete');
-  	radio.option('Plate');
-  	radio.position(0, 0);
-  	
-  //each side of the canvas
-  leftSide = createGraphics(400, 400);
-  rightSide = createGraphics(400, 400);
-  
+  createCanvas(900, 500);
   rawImpulse = loadSound('assets/' + cVerb.impulses[currentIR].name);
 
   // disconnect from master output...
@@ -46,83 +29,41 @@ function setup() {
 }
 
 function draw() {
-	
-	drawLeft();
-	drawRight();
-	
-	image(leftSide, 400, 0);
-	image(rightSide, 0, 0);
-	
-	chosenImpulse();
-	
-}
-
-function drawLeft() {
-  leftSide.background(30);
-  leftSide.fill(0, 255, 40);
+  background(30);
+  
+	textSize(20);
+	text('Current Impulse Response: ' + cVerb.impulses[currentIR].name, 400, 30);
+	fill(255);
+  
   let spectrum = fft.analyze();
   // Draw every value in the frequencySpectrum array as a rectangle
-  leftSide.noStroke();
+  noStroke();
   for (let i = 0; i < spectrum.length; i++) {
-    let x = map(i, 0, spectrum.length, 0, width);
+    let x = map(i, 0, spectrum.length, 0, (width/2));
     let h = -height + map(spectrum[i], 0, 255, height, 0);
-    leftSide.rect(x, height, width / spectrum.length, h);
-  }  
+    rect(x, height, (width/2) / spectrum.length, h);
+  }
+  
 }
 
-function drawRight() {
-	rightSide.background(100);
-	
-	button = createButton('Stop all');
-  	button.position(19, 100);
-  	
-  	button = createButton('Play');
-  	button.position(90, 100);
-  	
-  	button = createButton('Play Spring impulse only');
-  	button.position(19, 120);
-  	
-  	button = createButton('Play Small Plate impulse only');
-  	button.position(19, 140);
-  	
-  	button = createButton('Play Concrete Tunnel impulse only');
-  	button.position(19, 160);
-  	
-  	button = createButton('Play main sound only');
-  	button.position(19, 180);
-  	
-  	
-}
+function mousePressed() {
+  // cycle through the array of cVerb.impulses
+  currentIR++;
+  if (currentIR >= cVerb.impulses.length) {
+    currentIR = 0;
+  }
+  cVerb.toggleImpulse(currentIR);
 
-function chosenImpulse() {
-
-let val = -10;
-
-switch(radio.value()) {
-	
-	case 'Spring':
-		val = 2;
-		break;
-	case 'Concrete':
-		val = 1;
-		break;
-	case 'Plate':
-		val = 0;
-		break;
-	default:
-		val = -10;
-}
-
-if(val >= 0) {
-	currentIR = val;
-   	cVerb.toggleImpulse(currentIR);
-  	// play the sound through the impulse
-  	sound.play();
-  	
-  	}
+  // play the sound through the impulse
+  sound.play();
 
   // display the current Impulse Response name (the filepath)
-  console.log('Convolution Impulse Response: ' + cVerb.impulses[currentIR].name);
+  println('Convolution Impulse Response: ' + cVerb.impulses[currentIR].name);
 
   rawImpulse.setPath('assets/' + cVerb.impulses[currentIR].name);
+}
+
+// play the impulse (without convolution)
+function keyPressed() {
+  rawImpulse.play();
 }
